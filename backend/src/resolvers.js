c0f0069046bf607a9fc67e5bf1da0b028b50d1af
Henry <huyearly@gmail.com>
2020-07-11 19:14:30 -0600

    Clean  up before switching to typescript

--- backend/src/resolvers.js
+++
@@ -1,6 +0,0 @@
-const { fileLoader, mergeResolvers } = require("merge-graphql-schemas");
-const path = require("path");
-
-const resolversArray = fileLoader(path.join(__dirname, "./**/*.resolver.*"));
-
-exports.resolvers = mergeResolvers(resolversArray);
--- backend/src/typedefs.js
+++
@@ -1,6 +0,0 @@
-const { fileLoader, mergeTypes } = require("merge-graphql-schemas");
-const path = require("path");
-
-const typesArray = fileLoader(path.join(__dirname, "./**/*.graphql.*"));
-
-exports.typeDefs = mergeTypes(typesArray, { all: true });
