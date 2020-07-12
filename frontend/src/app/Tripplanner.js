2c26d5392d504209d870a7044f624a342aebe8ed
Henry <huyearly@gmail.com>
2020-07-12 14:54:26 -0600

    Add tripplanner component

---
+++ frontend/src/app/Tripplanner.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Header from "./header/Header";
+class Tripplanner extends Component {
+  render() {
+    return (
+      <div>
+        <Header></Header>
+      </div>
+    );
+  }
+}
+
+export { Tripplanner };
