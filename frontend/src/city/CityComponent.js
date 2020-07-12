c38f93288c11e4ea4c5278bfd7756eca11bf9668
Henry <huyearly@gmail.com>
2020-07-12 17:49:47 -0600

    Add City Component

---
+++ frontend/src/city/CityComponent.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./CityComponent.css";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+const GET_CITY = gql`
+  query GetCityInfo($id: ID!) {
+    City_getCityInfo(ID: $id) {
+      ID
+      Name
+      Description
+    }
+  }
+`;
+
+export default function CityComponent(props) {
+  const { loading, error, data } = useQuery(GET_CITY, {
+    variables: { id: props.cityID },
+  });
+  if (error) return <h1>Something went wrong!</h1>;
+  if (loading) return <h1>Loading...</h1>;
+
+  return <div></div>;
+}
+
+export { CityComponent };
